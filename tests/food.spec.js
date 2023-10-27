d08c92dade2056fa00cbfa4b3e842539a71c511f
Ivan <ivanaires32@gmail.com>
2023-10-27 16:12:14 -0300

    fix: code food

---
+++ tests/food.spec.js
@@ -0,0 +1,28 @@
+import supertest from "supertest";
+import app from "../src/app.js"
+import { faker } from "@faker-js/faker"
+import httpStatus from "http-status";
+
+const server = supertest(app)
+
+describe('GET /food', () => {
+    test('should return 204 when not exist name food', async () => {
+        const response = await server.get(`/food?name=${faker.lorem.word()}`)
+
+        expect(response.status).toBe(httpStatus.NO_CONTENT)
+        expect(response.body).toHaveLength(0)
+    });
+
+    test('should return 204 when not exist code food', async () => {
+        const response = await server.get(`/food?code=${faker.lorem.word()}`)
+
+        expect(response.status).toBe(httpStatus.NO_CONTENT)
+        expect(response.body).toHaveLength(0)
+    });
+
+    test('should return 200 when find all food', async () => {
+        const response = await server.get('/food')
+
+        expect(response.status).toBe(httpStatus.OK)
+    });
+})
