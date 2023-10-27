548843e3287c661466942a3053dbdce0d1e06060
Ivan <ivanaires32@gmail.com>
2023-10-27 15:06:57 -0300

    chore: config database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
 import httpStatus from "http-status";
+import { loadEnv } from "./config/envs";
+
+loadEnv()
+
 const app = express()
 
 app.use(express.json())
