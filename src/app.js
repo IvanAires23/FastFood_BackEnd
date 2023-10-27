1e3dc30e12846486a0de6b0df5cd3a20e7b9938c
Ivan <ivanaires32@gmail.com>
2023-10-26 23:18:11 -0300

    feat: first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import httpStatus from "http-status";
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/health', (req, res) => res.status(httpStatus.OK).send("I'm ok!"))
+
+export default app
