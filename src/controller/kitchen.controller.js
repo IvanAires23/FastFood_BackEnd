fd8b14d8f03bab8436d764fe50e10671b326802e
Ivan <ivanaires32@gmail.com>
2023-11-02 18:01:59 -0300

    fix: remove badrequest in kitchen.controller

--- src/controller/kitchen.controller.js
+++ src/controller/kitchen.controller.js
@@ -6,8 +6,6 @@ async function create(req, res) {
         const kitchen = await kitchenService.create(req.body)
         return res.status(httpStatus.CREATED).send(kitchen)
     } catch (err) {
-        if (err.name === 'badRequest')
-            return res.status(httpStatus.BAD_REQUEST).send(err)
         if (err.name === 'notFound')
             return res.status(httpStatus.NOT_FOUND).send(err)
         return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR)
