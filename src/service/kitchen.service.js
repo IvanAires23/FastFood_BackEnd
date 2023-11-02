be1597e4a86446dd3b4c490ee9171685a1c499a4
Ivan <ivanaires32@gmail.com>
2023-11-02 00:04:57 -0300

    fix: database seed

--- src/service/kitchen.service.js
+++ src/service/kitchen.service.js
@@ -3,7 +3,7 @@ import kitchenRepository from '../repository/kitchen.repository.js';
 
 async function create(body){
     if(body.name === '') throw { name: 'badRequest', message: 'Your name is required' };
-    else if(body.payment === '') throw {name: 'badRequest', message: 'Payment is required'};
+    else if(body.payment === '') throw {name: 'badRequest', message: 'Method payment is required'};
 
     const food = await foodRepository.findById(body.foodId);
     if(!food) throw { name: 'notFound', message: 'Food not found' };
