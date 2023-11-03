5a7d1589114ddb3ee6e84cb2f58df236afc5625b
Ivan <ivanaires32@gmail.com>
2023-11-03 09:40:39 -0300

    fix: code hamburguer in seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -25,7 +25,7 @@ async function createDefaultFood() {
                     subDescription: '2x hambúrguer 200g',
                     image: 'https://www.incrivel.com/_next/image/?url=https%3A%2F%2Fincrivel-prd.adtsys.com.br%2Fwp-content%2Fuploads%2F2022%2F11%2Fburger_carne_incri%CC%81vel.png&w=828&q=75',
                     price: 3050,
-                    code: '201',
+                    code: '200',
                 },
                 {
                     name: 'Batata Frita',
